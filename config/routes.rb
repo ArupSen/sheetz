807a5302dbef0622d0105fc652c359aeebde6443
Arup Sen <arup@arupdesigns.co.uk>
2020-05-21 11:00:20 +0100

    Generate customer model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "sheets#index"
 
   resources :sheets
+
+  resources :customers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_100154) do
+ActiveRecord::Schema.define(version: 2020_05_20_135510) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sheets", force: :cascade do |t|
     t.string "customer"
