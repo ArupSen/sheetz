181b4ddb31880a17645a01e9ab185a05ac8c1e32
Arup Sen <arup@arupdesigns.co.uk>
2020-05-21 11:54:17 +0100

    Create seeds file for Customer table and seed it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Customer.create!([
+  {
+    first_name: 'Stephen',
+    last_name: 'Trickett',
+    email: 'trickett.stephen@btinternet.com',
+    phone: '07905533789',
+    notes: 'Likes robust touring wheels'
+  },
+  {
+    first_name: 'Toby',
+    last_name: 'Collins',
+    email: 'toby_collins@tiscali.co.uk',
+    phone: '',
+    notes: 'deals in classic and vintage'
+  },
+  {
+    first_name: 'Adam',
+    last_name: 'Hardy',
+    email: 'unit113@gmail.com',
+    phone: '',
+    notes: 'barefoot walking and running'
+  },
+  {
+    first_name: 'Howard',
+    last_name: 'Stredwick',
+    email: 'howard@stredwick.co.uk',
+    phone: '447870238428',
+    notes: 'came for a lesson. Likes cats.'
+  },
+  {
+    first_name: 'Luke',
+    last_name: 'Morris',
+    email: 'luke.morris@nhs.net',
+    phone: '07903 911029',
+    notes: 'Audax rider'
+  },
+])
