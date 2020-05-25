aa947e8c5a8c395e2b829d9384212000bea3fa3a
Arup Sen <arup@arupdesigns.co.uk>
2020-05-25 11:49:01 +0100

    Set update action and full name helper

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,4 +8,17 @@ class CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone, :notes)
+  end
 end
--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,5 @@
 module CustomersHelper
+  def full_name(customer)
+    customer.first_name + " " + customer.last_name
+  end
 end
